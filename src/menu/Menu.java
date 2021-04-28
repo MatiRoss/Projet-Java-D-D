f646effe334851abfdc1dbbe8ec62cb0efbae173
MatiRoss <mati04.ross@gmail.com>
2021-04-28 20:13:07 +0200

    feat: Menu with character selection option

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -7,40 +7,40 @@ import classes.Magicien;
 
 public class Menu {
 
-	public Menu() {
-		// TODO Auto-generated constructor stub
-	}
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner clavier = new Scanner(System.in);
+        System.out.println("Quelle personnage souhaitez-vous incarner? Taper 1 pour choisir un guerrier, taper 2 pour choisir un magicien");
 
-		System.out.println(
-				"Quelle personnage souhaitez-vous incarner? Taper 1 pour choisir un guerrier, taper 2 pour choisir un magicien");
+        boolean choice = true;
 
-		int playerCharacter = clavier.nextInt();
-		clavier.nextLine();
+        while (choice) {
+            int playerCharacter = clavier.nextInt();
 
-		if (playerCharacter == 1 || playerCharacter == 2) {
-			if (playerCharacter == 1) {
-				System.out.println("Veuillez choisir le nom de votre personnage");
-				String playerName = clavier.next();
-				Guerrier guerrier = new Guerrier(playerName);
+            if (playerCharacter == 1) {
+                System.out.println("Veuillez taper le nom de votre personnage :");
 
-				System.out.println(guerrier.toString());
-			} else {
-				System.out.println("Veuillez taper 1 ou 2, et pas autre chose!!");
+                String playerName = clavier.next();
 
-			}
-			if (playerCharacter == 2) {
-				System.out.println("Veuillez choisir le nom de votre personnage");
-				String playerName = clavier.next();
-				Magicien magicien = new Magicien(playerName);
+                Guerrier guerrier = new Guerrier(playerName);
 
-				System.out.println(magicien.toString());
-				System.out.println(magicien.toString());
-			}
+                System.out.println(guerrier.toString());
 
-		}
-	}
+                choice = false;
+            } else if (playerCharacter == 2) {
+                System.out.println("Veuillez taper le nom de votre personnage :");
 
+                String playerName = clavier.next();
+
+                Magicien magicien = new Magicien(playerName);
+
+                System.out.println(magicien.toString());
+
+                choice = false;
+            } else {
+                System.out.println("Veuillez taper 1 ou 2, et pas autre chose!!");
+            }
+
+        }
+    }
 }
